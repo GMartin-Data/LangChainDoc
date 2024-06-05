9f02aab75a313e86b69eeeeb33d52a4f5051307e
GMartin-Data <gregory.martin.data@gmail.com>
2024-06-05 14:52:23 +0200

    Added chat components

--- chat_with_website/app.py
+++ chat_with_website/app.py
@@ -7,3 +7,14 @@ st.title("Chat with websites")
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
+
+st.chat_input("Type your message here...")
+
+with st.chat_message("AI"):
+    st.write("Hello, how can I help you?")
+
+with st.chat_message("Human"):
+    st.write("I want to know about LangChain.")
+
+with st.chat_message("AI"):
+    st.write("I don't know!")
