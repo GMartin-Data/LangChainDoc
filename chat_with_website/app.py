abf79fa3f782dcd3632647979289bf27a47e0050
GMartin-Data <gregory.martin.data@gmail.com>
2024-06-05 15:00:40 +0200

    Implement mock get_response function

--- chat_with_website/app.py
+++ chat_with_website/app.py
@@ -1,17 +1,25 @@
 import streamlit as st
 
-st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 
+# Functions
+def get_response(user_input: str):
+    return "I don't know!"
+
+# App config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with websites")
 
+# Sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
+# User input
 user_query = st.chat_input("Type your message here...")
 if user_query:
+    response = get_response(user_query)
     with st.chat_message("Human"):
         st.write(user_query)
 
     with st.chat_message("AI"):
-        st.write("I don't know!")
+        st.write(response)
