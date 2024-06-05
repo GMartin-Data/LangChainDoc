92b06c2d7acea042ffa9dfd0010a4df237df5e8c
GMartin-Data <gregory.martin.data@gmail.com>
2024-06-05 15:18:06 +0200

    Made chat history persistent

--- chat_with_website/app.py
+++ chat_with_website/app.py
@@ -10,9 +10,10 @@ def get_response(user_input: str):
 # App config
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with websites")
-chat_history = [
-    AIMessage("Hello, I am a bot, how can I help you?"),
-]
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage("Hello, I am a bot, how can I help you?"),
+    ]
 
 # Sidebar
 with st.sidebar:
@@ -23,9 +24,9 @@ with st.sidebar:
 user_query = st.chat_input("Type your message here...")
 if user_query:
     response = get_response(user_query)
-    chat_history.append(HumanMessage(content=user_query))
-    chat_history.append(AIMessage(content=response))
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
 
 with st.sidebar:
-    st.write(chat_history)
+    st.write(st.session_state.chat_history)
 
